5145ab39c2895c6970a5fc2f0609b206e921d269
Sibusiso Mayalo <mylsbu001@myuct.ac.za>
2019-09-10 00:05:38 +0200

    Removed method set_type

--- Node.py
+++ Node.py
@@ -1,17 +1,14 @@
 class Node:
 
-    def __init__(self, nodeID=str(), nodeType=str() ):
+    def __init__(self, nodeID=str(), type=str() ):
         self.ID=nodeID
-        self.type=self.set_type(nodeType)
+        self.type=type
         self.edges=dict()
-
-    def set_type(self, new_type):
-        self.type = new_type
 
     def add_edge(self, destination, cost, delay, rel_type):
         temp_list = list()
         temp_list.append(cost)
         temp_list.append(delay)
+        temp_list.append(rel_type)
 
-        self.set_type(rel_type)
         self.edges[destination] = temp_list
